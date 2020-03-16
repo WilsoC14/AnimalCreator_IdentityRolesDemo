46e0a1b52eaa1c83b2ade8166f3d67935577782c
Casey Wilson <caswil10@gmail.com>
2020-03-16 19:38:11 -0400

    Add project files.

---
+++ AnimalCreator.WebAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace AnimalCreator.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
