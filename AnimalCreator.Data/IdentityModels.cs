3a942ba5e7d22b456638bc7d0ce900c7b7ec41b7
Casey Wilson <caswil10@gmail.com>
2020-03-16 20:54:06 -0400

    Created Animal Class, Brought in Entity using statement in Identity Models

--- AnimalCreator.Data/IdentityModels.cs
+++ AnimalCreator.Data/IdentityModels.cs
@@ -1,4 +1,6 @@
-﻿using System.Security.Claims;
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
@@ -28,6 +30,23 @@ namespace AnimalCreator.Data
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        public DbSet<Animal> Animals { get; set; } //<--- Add this
+
+    }
+    public class IdentityUserLoginConfiguration : EntityTypeConfiguration<IdentityUserLogin>
+    {
+        public IdentityUserLoginConfiguration()
+        {
+            HasKey(iul => iul.UserId);
+        }
+    }
+    public class IdentityUserRoleConfiguration : EntityTypeConfiguration<IdentityUserRole>
+    {
+        public IdentityUserRoleConfiguration()
+        {
+            HasKey(iur => iur.UserId);
         }
     }
 }
